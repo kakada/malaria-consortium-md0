27c076b6a04a74824eb1572aae17512893b51e3d
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-05-30 11:59:45 +0700

    Small refactor

--- app/controllers/reports_concern.rb
+++ app/controllers/reports_concern.rb
@@ -44,7 +44,7 @@ module ReportsConcern
         @users.push :place => @place.class, :users => @place.users
 
         options = {}
-        options.send(:[]=, "#{@place.class.to_s.tableize.singularize}_id", @place.id)
+        options["#{@place.class.to_s.tableize.singularize}_id"] = @place.id
         types = Place::Types.from(Place::Types.index(@place.class.to_s) + 1).each do |type|
           options[:place_class] = type
           @users.push :place => type.constantize, :count => User.where(options).count

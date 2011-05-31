79f9354c7e4f84ef710d5429d855f45b23155faf
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-05-31 15:29:19 +0700

    Don't validate password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :password_length => 1..128
 
   # Setup accessible (or protected) attributes for your model
   #attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -179,6 +179,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  
+
 
 end

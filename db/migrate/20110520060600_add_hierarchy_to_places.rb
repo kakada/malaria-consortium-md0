02a80f412c187a56db975715780278e2351a3cb2
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-05-31 15:10:34 +0700

    Fixed broken migration

--- db/migrate/20110520060600_add_hierarchy_to_places.rb
+++ db/migrate/20110520060600_add_hierarchy_to_places.rb
@@ -1,16 +1,6 @@
 class AddHierarchyToPlaces < ActiveRecord::Migration
   def self.up
     add_column :places, :hierarchy, :string
-
-    [Province, OD, HealthCenter, Village].each do |place_class|
-      puts "Setting hierarchy ids for #{place_class.name}(s)"
-      place_class.reset_column_information
-      places = place_class.includes(:parent).all.to_a
-      places.each_with_index do |place, i|
-        place.save
-        STDOUT.write "#{i}/#{places.count}\r"
-      end
-    end
   end
 
   def self.down

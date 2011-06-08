70a854405864e8bc09c330421bb43faa41a3719a
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-06-08 14:04:00 +0700

    Correct time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_filter :authenticate_admin!
+  before_filter :set_cambodia_time_zone
+
+  private
+
+  def set_cambodia_time_zone
+    Time.zone = "Bangkok" # Same time zone as Cambodia
+  end
 end

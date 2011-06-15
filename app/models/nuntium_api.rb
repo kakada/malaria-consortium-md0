215de247f97897d3d3207eeede33a76dabf8ad3f
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-06-15 17:10:06 +0700

    Removed unused method

--- app/models/nuntium_api.rb
+++ app/models/nuntium_api.rb
@@ -2,8 +2,4 @@ class NuntiumAPI
   def initialize service_url, account_name, application_name, application_password
     @api = Nuntium.new service_url, account_name, application_name, application_password
   end
-
-  def send_sms message
-    @api.send_ao message
-  end
 end

d48393572eceb3ee5081810eaf343d9c7f24644a
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-06-16 12:54:50 +0700

    Removed unused method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -45,63 +45,6 @@ class PlacesController < ApplicationController
     @country = Country.first
   end
 
-  #GET places/map_report
-  def map_report
-    from = params[:from]
-    to = params[:to]
-    if params[:id].blank? || params[:id].to_i == 0
-        total = Report.between_dates(from, to).count
-        country = Country.first
-        places = [{
-                    "name" => country.name,
-                    "id" => country.id,
-                    "type" => country.type,
-                    "parent_id" => country.parent_id,
-                    "lat" => country.lat,
-                    "lng" => country.lng
-                  }]
-        creteria = Creteria.new
-
-        places.each do |place|
-          place["total"] = total
-          creteria.add_record!(place["name"], place["total"])
-        end
-        creteria.prepare!
-        clouds = creteria.cloud
-        @result = {:place=>places, :cloud=>clouds }
-      else
-        place = Place.find(params[:id])
-        if(place.type == "Country")
-          sql = " SELECT province.*, count(report.id) as total FROM places province LEFT JOIN (SELECT * from reports WHERE created_at between '#{from}' AND '#{to}') AS report " +
-              " ON province.id = report.province_id  where province.parent_id = #{place.id} GROUP BY province.id ORDER BY total DESC "
-        elsif(place.type == "Province")
-          sql = " SELECT od.*, count(report.id) as total FROM places od LEFT JOIN (SELECT * from reports WHERE created_at between '#{from}' AND '#{to}') AS  report " +
-                " ON od.id = report.od_id WHERE  od.parent_id = #{place.id} GROUP BY od.id ORDER BY total DESC "
-        elsif(place.type == "OD" )
-          sql = " SELECT hc.*, count(report.id) as total FROM places hc LEFT JOIN (SELECT * from reports WHERE created_at between '#{from}' AND '#{to}') AS report " +
-                " ON hc.id = report.health_center_id WHERE hc.parent_id = #{place.id} GROUP BY hc.id ORDER BY total DESC "
-        elsif(place.type == "HealthCenter")
-          sql = " SELECT village.*, count(report.id) as total FROM places village LEFT JOIN (SELECT * from reports WHERE created_at between '#{from}' AND '#{to}') AS  report " +
-                " ON village.id = report.village_id WHERE village.parent_id = #{place.id} GROUP BY village.id ORDER BY total DESC "
-        elsif(place.type == "Village")
-          sql = " SELECT village.*, count(report.id) as total FROM places village LEFT JOIN (SELECT * from reports WHERE created_at between '#{from}' AND '#{to}') AS report " +
-                " ON village.id = report.village_id WHERE village.id = #{place.id} GROUP BY village.id ORDER BY total DESC "
-        end
-
-        places = Place.connection.select_all(sql)
-        creteria = Creteria.new
-        places.each do |place|
-          creteria.add_record!(place["name"], place["total"])
-        end
-
-        creteria.prepare!
-        clouds = creteria.cloud
-        @result =  {:place =>places, :cloud=> clouds, :parent => {:type=>place.type,:id=>place.id,:name=>place.name} }
-
-    end
-    render :json =>@result
-  end
-
   def autocomplete
     places = Place.search_for_autocomplete params[:query]
     places = places.where(:type => params[:type]) if params[:type].present?

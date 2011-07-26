cc6599915869384610bbf3b6422e5e09c6cd4ec4
lychannainf <chen.webdev@gmail.com>
2011-07-26 14:51:18 +0700

    dont launch exception when removing index  in migration

--- db/migrate/20110622044657_add_not_ignored_on_reports_index.rb
+++ db/migrate/20110622044657_add_not_ignored_on_reports_index.rb
@@ -2,20 +2,27 @@ class AddNotIgnoredOnReportsIndex < ActiveRecord::Migration
   Types = :country, :province, :od, :health_center, :village, :place
 
   def self.up
-    Types.each do |type|
-      remove_index :reports, ["#{type}_id", :error]
-      add_index :reports, ["#{type}_id", :error, :ignored]
+    begin
+      Types.each do |type|
+        remove_index :reports, ["#{type}_id", :error]
+        add_index :reports, ["#{type}_id", :error, :ignored]
+      end
+      remove_index :reports, [:error, :village_id, :created_at]
+      add_index :reports, [:error, :ignored, :village_id, :created_at]
+    rescue
     end
-    remove_index :reports, [:error, :village_id, :created_at]
-    add_index :reports, [:error, :ignored, :village_id, :created_at]
+
   end
 
   def self.down
-    Types.each do |type|
-      remove_index :reports, ["#{type}_id", :error, :ignored]
-      add_index :reports, ["#{type}_id", :error]
+    begin
+      Types.each do |type|
+        remove_index :reports, ["#{type}_id", :error, :ignored]
+        add_index :reports, ["#{type}_id", :error]
+      end
+      remove_index :reports, [:error, :ignored, :village_id, :created_at]
+      add_index :reports, [:error, :village_id, :created_at]
+    rescue
     end
-    remove_index :reports, [:error, :ignored, :village_id, :created_at]
-    add_index :reports, [:error, :village_id, :created_at]
   end
 end
